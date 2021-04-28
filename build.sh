129e120faf52e2d7cd02ab116dc04932d81496ed
Vicc2008 <66806243+Vicc2008@users.noreply.github.com>
2021-04-28 18:16:11 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -e
 . build_common.sh
 # not actually GCC5; it's GCC7 on Ubuntu 18.04.
-GCC5_AARCH64_PREFIX=aarch64-linux-gnu- build -s -n 0 -a AARCH64 -t GCC5 -p AsusMaxProM1Pkg/AsusMaxProM1Pkg.dsc
-gzip -c < workspace/Build/AsusMaxProM1Pkg/DEBUG_GCC5/FV/ASUSMAXPROM1PKG_UEFI.fd >uefi_image
-cat X00TD.dtb >>uefi_image
+GCC5_AARCH64_PREFIX=aarch64-linux-gnu- build -s -n 0 -a AARCH64 -t GCC5 -p TroikaPkg/TroikaPkg.dsc
+gzip -c < workspace/Build/TroikaPkg/DEBUG_GCC5/FV/TROIKAPKG_UEFI.fd >uefi_image
+cat troika.dtb >>uefi_image
 abootimg --create uefi.img -k uefi_image -r ramdisk-null -f bootimg.cfg
